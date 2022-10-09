07340648aa52bc93b0ed19c94bc2ab6ac96ee8db
capg84 <cyprian.gomes@live.co.uk>
2022-10-09 18:48:04 +0100

    added seed.js to run the seeds

---
+++ seeds/seed.js
@@ -0,0 +1,33 @@
+const sequelize = require("../config/connection");
+const { User, Blog, Comment } = require("../models");
+
+const userData = require("./user.json");
+const blogData = require("./blogs.json");
+const commentData = require("./comments.json");
+
+const seed = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seed();
