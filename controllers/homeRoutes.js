933fe3e7aaf171569ac742b84a486ec7bb664b1b
capg84 <cyprian.gomes@live.co.uk>
2022-12-07 12:03:58 +0000

    updated the homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,9 +11,9 @@ router.get("/sign-up", async (req, res) => {
 });
 
 // Rendering sign-up sign-in prompt
-router.get("/signin-signup", async (req, res) => {
+router.get("/signinSignup", async (req, res) => {
   try {
-    res.render("signprompt");
+    res.render("signPrompt");
   } catch (err) {
     res.status(500).json(err);
   }
@@ -23,6 +23,32 @@ router.get("/signin-signup", async (req, res) => {
 router.get("/sign-in", async (req, res) => {
   try {
     res.render("signin");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// rendering all blogs to home page
+router.get("/", async (req, res) => {
+  try {
+    const blogs = await Blog.findAll({
+      attributes: ["id", "title", "blog", "date_created", "user_id"],
+      include: [{ model: User, attributes: ["username"] }],
+    });
+
+    const blogsSerialized = blogs.map((blog) => blog.get({ plain: true }));
+    const obj = { blogs: blogsSerialized, logged_in: req.session.logged_in };
+
+    res.render("saved-blogs", obj);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// rendering the new blog page
+router.get("/new-blog", async (req, res) => {
+  try {
+    res.render("new-blog");
   } catch (err) {
     res.status(500).json(err);
   }
